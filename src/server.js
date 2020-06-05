28114534699335544f010e03622188dd5c6c5e06
pedro henrique <ph8464803@gmail.com>
2020-06-05 15:30:59 -0300

    frist

--- src/server.js
+++ src/server.js
@@ -3,14 +3,23 @@ const app = express()
 
 app.use(express.static("public"))
 
+const nunjunck = require('nunjucks')
+nunjunck.configure("src/views", {
+  express: app,
+  noCache: true
+})
+
+
+
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
+  return res.render("index.html")
 })
 app.get('/creat-point.html', (req, res) => {
-  res.sendFile(__dirname + "/views/creat-point.html")
+  return res.render("creat-point.html")
 })
-app.get('/search-result.html', (req, res) => {
-  res.sendFile(__dirname + "/views/search-result.html")
+app.get('/search', (req, res) => {
+  return res.render("search-result.html")
 })
 
 
