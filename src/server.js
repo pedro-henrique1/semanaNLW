b7418f3efde8660b155c9be017374ccbc16e5758
pedro henrique <ph8464803@gmail.com>
2020-06-09 18:17:07 -0300

    terminei

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const db = require("./database/db");
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
 const nunjunck = require("nunjucks");
 nunjunck.configure("src/views", {
   express: app,
@@ -14,13 +16,54 @@ nunjunck.configure("src/views", {
 app.get("/", (req, res) => {
   return res.render("index.html");
 });
-app.get("/creat-point.html", (req, res) => {
-  console.log(req.query);
-
-  return res.render("creat-point.html");
+app.get("/creat-point", (req, res) => {
+  return res.render("creat-point.html", { saved: true });
 });
+
+app.post("/save", function(req, res) {
+  const query = `
+  INSERT INTO places (
+    image,
+    name,
+    address,
+    address2,
+    state,
+    city,
+    items
+  ) VALUES (?,?,?,?,?,?,?);
+  `;
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ];
+  function afterInsretData(err) {
+    if (err) {
+      return res.send("Erro no cadastro");
+      console.log(err);
+    }
+    console.log("Cadasstro com sucesso");
+    console.log(this);
+    res.render("creat-point.html", { saved: true });
+  }
+  db.run(query, values, afterInsretData);
+});
+
 app.get("/search", (req, res) => {
-  db.all(`SELECT * FROM places`, function(err, rows) {
+  const search = req.query.search;
+  if (search == "") {
+    return res.render("search-results.html", { total: 0 });
+  }
+
+  db.all(`SELECT * FROM places WHERE city LIKE = '%${search}%'`, function(
+    err,
+    rows
+  ) {
     if (err) {
       return console.log(err);
     }
@@ -30,4 +73,4 @@ app.get("/search", (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(9000);
